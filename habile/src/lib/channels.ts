b236643689011f37a6575e684f9c03903023c92c
Clembs <clembs@clembs.com>
2023-09-25 21:14:27 +0200

    feat: move channels/prompts/limits to new files

---
+++ habile/src/lib/channels.ts
@@ -0,0 +1,4 @@
+export const allowedChannels = [
+  '1155549814705111050', // Habile Chat 1
+  '1155812218840027167', // Habile Chat 2
+];
---
+++ habile/src/lib/usageLimits.ts
@@ -0,0 +1,11 @@
+export const firstUserUsageWarning = 0.3;
+export const secondUserUsageWarning = 0.5;
+export const userUsageLimit = 0.6;
+
+export const firstGlobalUsageWarning = 5;
+export const secondGlobalUsageWarning = 6;
+export const globalUsageLimit = 7;
+
+export const totalCredit = 8;
+
+export const averageUsageCost = 0.035;
