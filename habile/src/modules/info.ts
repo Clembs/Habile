4d13078b223d40d808ac13e1c4700bd1a4f450bd
Clembs <clembs@clembs.com>
2023-09-25 21:21:41 +0200

    feat: separate leaderboard and individual usage

--- habile/src/modules/info.ts
+++ habile/src/modules/info.ts
@@ -1,29 +1,18 @@
 import dedent from 'dedent';
-import { readFileSync, readdirSync } from 'fs';
+import { readFileSync } from 'fs';
 import { ChatCommand } from 'purplet';
 import { GlobalUsage, UserData } from '../lib/types';
+import { timestampMention } from '@purplet/utils';
+import { averageUsageCost } from '../lib/usageLimits';
 
 export default ChatCommand({
   name: 'usage',
-  description: 'Know how much you and others have used Habile Chat.',
+  description: 'Get useful info about your Habile Chat credit usage.',
   async handle() {
-    let currentUserSpending = 0;
-    const usersSpending: { id: string; spent: number }[] = [];
-
     const totalUsage: GlobalUsage = JSON.parse(readFileSync('./static/usage.json', 'utf-8'));
-    const users = readdirSync('./static/users');
-
-    users.forEach((u) => {
-      u = u.replace('.json', '');
-
-      const { spent } = JSON.parse(readFileSync(`./static/users/${u}.json`, 'utf-8')) as UserData;
-
-      usersSpending.push({ id: u, spent });
-
-      if (u === this.user.id) {
-        currentUserSpending = spent;
-      }
-    });
+    const userUsage: UserData = JSON.parse(
+      readFileSync(`./static/users/${this.user.id}.json`, 'utf-8'),
+    );
 
     this.reply({
       content: dedent`
@@ -31,19 +20,13 @@ export default ChatCommand({
       **Total spent:** $${Number(totalUsage.used).toFixed(3)} / $${totalUsage.total.toFixed(
         3,
       )} ($${(totalUsage.total - totalUsage.used).toFixed(3)} remaining - about ${Math.round(
-        (totalUsage.total - totalUsage.used) / 0.02,
+        (totalUsage.total - totalUsage.used) / averageUsageCost,
       )} messages)
       
-      **You spent:** $${currentUserSpending.toFixed(3)}
+      **You spent:** $${userUsage.spent.toFixed(3)}
       
-      **Leaderboard:**
-      ${usersSpending
-        .sort((a, b) => b.spent - a.spent)
-        .map(({ id, spent }) => `\\- <@${id}>: **$${spent.toFixed(3)}**`)
-        .join('\n')}
-        
-      Note: Each message costs around $0.02 to generate, depending on token (word/expression) usage.
-      Note 2: Per-user usage is calculated since since 2023-09-24 at 21:40 CET.
+      Note: a message costs ~$${averageUsageCost} to generate, based on token usage.
+      Note 2: user usage is calculated since ${timestampMention(new Date('2023-09-24T21:40:00'))}.
         `,
     });
   },
---
+++ habile/src/modules/leaderboard.ts
@@ -0,0 +1,43 @@
+import dedent from 'dedent';
+import { TextCommand } from 'purplet';
+import { readFileSync, readdirSync } from 'fs';
+import { GlobalUsage, UserData } from '../lib/types';
+import { userUsageLimit, firstUserUsageWarning, averageUsageCost } from '../lib/usageLimits';
+import { timestampMention } from '@purplet/utils';
+
+export default TextCommand({
+  name: 'lb',
+  async handle() {
+    const totalUsage: GlobalUsage = JSON.parse(readFileSync('./static/usage.json', 'utf-8'));
+    const users: [string, UserData][] = readdirSync('./static/users').map((u) => {
+      u = u.replace('.json', '');
+
+      return [u, JSON.parse(readFileSync(`./static/users/${u}.json`, 'utf-8'))];
+    });
+
+    this.reply({
+      content: dedent`
+      # Habile Chat - Global Usage
+      **Total spent:** $${Number(totalUsage.used).toFixed(3)} / $${totalUsage.total.toFixed(
+        3,
+      )} ($${(totalUsage.total - totalUsage.used).toFixed(3)} remaining - about ${Math.round(
+        (totalUsage.total - totalUsage.used) / averageUsageCost,
+      )} messages)
+
+      **Spending per user:**
+      ${users
+        .sort(([_, a], [__, b]) => b.spent - a.spent)
+        .map(
+          ([id, { spent }]) =>
+            `\\- <@${id}>: **$${spent.toFixed(3)}** ${
+              spent >= userUsageLimit ? '(`⛔`)' : spent >= firstUserUsageWarning ? '(`⚠️`)' : ''
+            }`,
+        )
+        .join('\n')}
+        
+      Note: a message costs ~$${averageUsageCost} to generate, based on token usage.
+      Note 2: user usage is calculated since ${timestampMention(new Date('2023-09-24T21:40:00'))}.
+        `,
+    });
+  },
+});
